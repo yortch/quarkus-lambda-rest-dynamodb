2258df9233d5db751344ba6e4121dbaeed7ed0e0
Jorge Balderas <jbalderas@redhat.com>
2023-04-13 09:21:59 -0400

    Adding static table schema init

--- src/main/java/com/quarkus/demo/FruitSyncService.java
+++ src/main/java/com/quarkus/demo/FruitSyncService.java
@@ -9,17 +9,31 @@ import javax.inject.Inject;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
 
 @ApplicationScoped
 public class FruitSyncService extends AbstractService {
 
     private DynamoDbTable<Fruit> fruitTable;
 
+    private static final StaticTableSchema<Fruit> FRUIT_TABLE_SCHEMA =
+        StaticTableSchema.builder(Fruit.class)
+            .newItemSupplier(Fruit::new)
+            .addAttribute(String.class, a -> a.name("fruitName")
+                                                .getter(Fruit::getName)
+                                                .setter(Fruit::setName)
+                                                .tags(StaticAttributeTags.primaryPartitionKey()))
+            .addAttribute(String.class, a -> a.name("description")
+                                                .getter(Fruit::getDescription)
+                                                .setter(Fruit::setDescription))
+            .build();
+
     @Inject
     FruitSyncService(DynamoDbEnhancedClient dynamoEnhancedClient) {
-        fruitTable = dynamoEnhancedClient.table(getTableName(),
-            TableSchema.fromClass(Fruit.class));
+        //NOTE: to improve initialization time we are using StaticSchema instead of inferring from bean
+        //TableSchema.fromClass(Fruit.class)
+        fruitTable = dynamoEnhancedClient.table(getTableName(), FRUIT_TABLE_SCHEMA);
     }
 
     public List<Fruit> findAll() {
